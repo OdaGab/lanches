11024ef80fcad6000e0573d2d860cf7f0f881c71
Odair <odavita32@gmail.com>
2026-01-07 16:08:20 -0300

    arquivo Globais cores

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const Homepage = () => {
   return (
     <div className="p-5 border border-red-500 rounded-xl">
       <h1 className="text-red-500">Odair Gabriel</h1>
+      <Button>Click me</Button>
+      <br /> <br />
+      <Input placeholder="Testando Input" />
     </div>
   );
 };
