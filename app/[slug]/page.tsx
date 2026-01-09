156f5572fa9bde382f03a0d259e8e8dd97a9d9e9
Odair <odavita32@gmail.com>
2026-01-08 23:30:10 -0300

    Primeira busca no DB nome da Loja

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,9 +1,14 @@
+import { db } from "@/lib/prisma";
+
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
 }
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
-  return <h1>{slug}</h1>;
+
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+
+  return <h1>{restaurant?.name}</h1>;
 };
 
 export default RestaurantPage;
