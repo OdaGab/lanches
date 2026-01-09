38d493186a08a79b63bd70732e8269c72b598f53
Odair <odavita32@gmail.com>
2026-01-08 22:59:09 -0300

    Passando parametros pela url

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,9 @@
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  return <h1>{slug}</h1>;
+};
+
+export default RestaurantPage;
