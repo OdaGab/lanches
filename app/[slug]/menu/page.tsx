229ca015aa65814f2a2aecfeb50f15a56322e8a2
Odair <odavita32@gmail.com>
2026-01-12 22:51:28 -0300

    Trabalhando passagem de parametros

--- app/[slug]/menu/page.tsx
+++ app/[slug]/menu/page.tsx
@@ -1,10 +1,33 @@
+import { db } from "@/lib/prisma";
+
 interface RestaurantmenuPageProps {
   params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod?: string }>;
 }
 
-const RestaurantmenuPage = async ({ params }: RestaurantmenuPageProps) => {
+const isConsumptionMethodValid = (consumptionMethod?: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod ?? "");
+};
+
+const RestaurantmenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantmenuPageProps) => {
   const { slug } = await params;
-  return <div>{slug}</div>;
+  const { consumptionMethod } = await searchParams;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  const valid = isConsumptionMethodValid(consumptionMethod);
+
+  return (
+    <div>
+      Menu page for restaurant {slug}{" "}
+      {restaurant?.name ? `- ${restaurant.name}` : ""} - method:{" "}
+      {valid ? consumptionMethod : "invalid"}
+    </div>
+  );
 };
 
 export default RestaurantmenuPage;
+
+//http://localhost:3000/fsw-donalds/menu?consumptionMethod=DINE_IN
+//http://localhost:3000/fsw-donalds/menu?consumptionMethod=123
