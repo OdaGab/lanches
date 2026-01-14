2244daf834380a8b2b620159afc2e1bb88db2c82
Odair <odavita32@gmail.com>
2026-01-14 12:23:20 -0300

    Cabeçalho do menu produtos

---
+++ app/[slug]/menu/components/categories.tsx
@@ -0,0 +1,29 @@
+import { Restaurant } from "@prisma/client";
+import Image from "next/image";
+
+interface RestaurantCategoriesProps {
+  restaurant: Restaurant;
+}
+
+const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
+  return (
+    <div className="mt-(-1.5rem) relative z-50 rounded-t-3xl border bg-white">
+      <div className="flex items-center gap-3 p-4">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          height={45}
+          width={45}
+        />
+        <div>
+          <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+          <p className="text-muted-foreground text-sm">
+            {restaurant.description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCategories;
